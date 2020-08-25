9aaa1e3783730b1ba5f393ccffdf2704fe04a6c6
LenaGritse <mironuk.ea@gmail.com>
2020-08-25 23:57:26 +0300

    IntentLesson4

--- app/src/main/java/com/lenagritse/myweatherapp/MainActivity2.java
+++ app/src/main/java/com/lenagritse/myweatherapp/MainActivity2.java
@@ -3,6 +3,8 @@ package com.lenagritse.myweatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Button backToMainActivity = findViewById(R.id.buttonBack);
+        backToMainActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
